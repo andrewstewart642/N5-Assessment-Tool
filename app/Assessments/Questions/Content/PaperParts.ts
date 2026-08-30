7500baafcbb0421688addc6c507687da9c4ebf12
andrewstewart642 <andrewstewart642@gmail.com>
2026-08-30 23:51:35 +0100

    Add straight-line graph paper part

--- app/Assessments/Questions/Content/PaperParts.ts
+++ app/Assessments/Questions/Content/PaperParts.ts
@@ -1,3 +1,24 @@
+export type LinearEquationGraphData = {
+  a: number;
+  b: number;
+  c: number;
+};
+
+export type StraightLineSystemGraphData = {
+  xVariable: string;
+  yVariable: string;
+  firstEquation: LinearEquationGraphData;
+  secondEquation: LinearEquationGraphData;
+  intersection: [number, number];
+  labelledIntersection: string;
+};
+
+export type StraightLineSystemGraphPart =
+  StraightLineSystemGraphData & {
+    kind: "straightLineSystemGraph";
+  };
+
 export type PaperPart =
   | { kind: "text"; value: string }
-  | { kind: "math"; latex: string; displayMode?: boolean };
+  | { kind: "math"; latex: string; displayMode?: boolean }
+  | StraightLineSystemGraphPart;
