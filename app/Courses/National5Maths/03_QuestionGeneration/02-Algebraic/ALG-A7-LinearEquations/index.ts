d4c69a767b62202212b54da2a5ae32bed4fded9c
andrewstewart642 <andrewstewart642@gmail.com>
2026-09-01 01:58:59 +0100

    Export A7 difficulty module

--- app/Courses/National5Maths/03_QuestionGeneration/02-Algebraic/ALG-A7-LinearEquations/index.ts
+++ app/Courses/National5Maths/03_QuestionGeneration/02-Algebraic/ALG-A7-LinearEquations/index.ts
@@ -1,6 +1,7 @@
 export * from "./Types";
 export * from "./Evidence";
 export * from "./Calibration";
+export * from "./Difficulty";
 export * from "./PromptGrammar";
 export * from "./Validation";
 export * from "./Generator";
