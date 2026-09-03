b5d20415b1165886e56d5a3675a37aaf43e688f1
andrewstewart642 <andrewstewart642@gmail.com>
2026-09-03 03:13:07 +0100

    Stage 3B project A8 evidence at catalog boundary

--- app/Courses/National5Maths/03_SkillCatalog/02-Algebraic/ALG-A8-SimultaneousEquations/HistoricalEvidence.ts
+++ app/Courses/National5Maths/03_SkillCatalog/02-Algebraic/ALG-A8-SimultaneousEquations/HistoricalEvidence.ts
@@ -9,6 +9,9 @@ import { N5_MATHS_2022_P2_Q4 } from "../../../01_QuestionCatalog/2022/Paper2/N5_
 import { N5_MATHS_2023_P1_Q3 } from "../../../01_QuestionCatalog/2023/Paper1/N5_Maths_2023_P1_Q3";
 import { N5_MATHS_2024_P1_Q7 } from "../../../01_QuestionCatalog/2024/Paper1/N5_Maths_2024_P1_Q7";
 import { N5_MATHS_2025_P2_Q10 } from "../../../01_QuestionCatalog/2025/Paper2/N5_Maths_2025_P2_Q10";
+import {
+  toHistoricalQuestionCatalogView,
+} from "../../../01_QuestionCatalog/QuestionCatalogHistoricalView";
 import { N5_MATHS_2014_P2_Q3_MS } from "../../../02_AnswerCatalog/2014/Paper2/N5_Maths_2014_P2_Q3_MS";
 import { N5_MATHS_2015_P1_Q11_MS } from "../../../02_AnswerCatalog/2015/Paper1/N5_Maths_2015_P1_Q11_MS";
 import { N5_MATHS_2016_P1_Q4_MS } from "../../../02_AnswerCatalog/2016/Paper1/N5_Maths_2016_P1_Q4_MS";
@@ -20,21 +23,32 @@ import { N5_MATHS_2022_P2_Q4_MS } from "../../../02_AnswerCatalog/2022/Paper2/N5
 import { N5_MATHS_2023_P1_Q3_MS } from "../../../02_AnswerCatalog/2023/Paper1/N5_Maths_2023_P1_Q3_MS";
 import { N5_MATHS_2024_P1_Q7_MS } from "../../../02_AnswerCatalog/2024/Paper1/N5_Maths_2024_P1_Q7_MS";
 import { N5_MATHS_2025_P2_Q10_MS } from "../../../02_AnswerCatalog/2025/Paper2/N5_Maths_2025_P2_Q10_MS";
+import {
+  toHistoricalAnswerCatalogView,
+} from "../../../02_AnswerCatalog/AnswerCatalogHistoricalView";
 import { createSkillHistoricalEvidenceSet } from "../../SkillCatalogTypes";
+
+const historicalPair = (
+  question: Parameters<typeof toHistoricalQuestionCatalogView>[0],
+  answer: Parameters<typeof toHistoricalAnswerCatalogView>[0],
+) => ({
+  question: toHistoricalQuestionCatalogView(question),
+  answer: toHistoricalAnswerCatalogView(answer),
+});
 
 export const A8_HISTORICAL_EVIDENCE = createSkillHistoricalEvidenceSet(
   "alg-a08-simultaneous-equations",
   [
-    { question: N5_MATHS_2014_P2_Q3, answer: N5_MATHS_2014_P2_Q3_MS },
-    { question: N5_MATHS_2015_P1_Q11, answer: N5_MATHS_2015_P1_Q11_MS },
-    { question: N5_MATHS_2016_P1_Q4, answer: N5_MATHS_2016_P1_Q4_MS },
-    { question: N5_MATHS_2017_P1_Q13, answer: N5_MATHS_2017_P1_Q13_MS },
-    { question: N5_MATHS_2018_P1_Q3, answer: N5_MATHS_2018_P1_Q3_MS },
-    { question: N5_MATHS_2019_P1_Q8, answer: N5_MATHS_2019_P1_Q8_MS },
-    { question: N5_MATHS_2021_P1_Q7, answer: N5_MATHS_2021_P1_Q7_MS },
-    { question: N5_MATHS_2022_P2_Q4, answer: N5_MATHS_2022_P2_Q4_MS },
-    { question: N5_MATHS_2023_P1_Q3, answer: N5_MATHS_2023_P1_Q3_MS },
-    { question: N5_MATHS_2024_P1_Q7, answer: N5_MATHS_2024_P1_Q7_MS },
-    { question: N5_MATHS_2025_P2_Q10, answer: N5_MATHS_2025_P2_Q10_MS },
+    historicalPair(N5_MATHS_2014_P2_Q3, N5_MATHS_2014_P2_Q3_MS),
+    historicalPair(N5_MATHS_2015_P1_Q11, N5_MATHS_2015_P1_Q11_MS),
+    historicalPair(N5_MATHS_2016_P1_Q4, N5_MATHS_2016_P1_Q4_MS),
+    historicalPair(N5_MATHS_2017_P1_Q13, N5_MATHS_2017_P1_Q13_MS),
+    historicalPair(N5_MATHS_2018_P1_Q3, N5_MATHS_2018_P1_Q3_MS),
+    historicalPair(N5_MATHS_2019_P1_Q8, N5_MATHS_2019_P1_Q8_MS),
+    historicalPair(N5_MATHS_2021_P1_Q7, N5_MATHS_2021_P1_Q7_MS),
+    historicalPair(N5_MATHS_2022_P2_Q4, N5_MATHS_2022_P2_Q4_MS),
+    historicalPair(N5_MATHS_2023_P1_Q3, N5_MATHS_2023_P1_Q3_MS),
+    historicalPair(N5_MATHS_2024_P1_Q7, N5_MATHS_2024_P1_Q7_MS),
+    historicalPair(N5_MATHS_2025_P2_Q10, N5_MATHS_2025_P2_Q10_MS),
   ],
 );
